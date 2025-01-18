628675e75c18da4c89bf3550e556bd2aff371ca3
Oleh Selivanov <my.mail.olech@gmail.com>
2025-01-18 18:34:19 +0100

    add retries

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   defaultCommandTimeout: 10000,
+  retries: 3,
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
