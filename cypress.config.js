554fda58322c6ceb5bcec89fec906a7bc3cc2751
Oleh Selivanov <76633585+OlleyO@users.noreply.github.com>
2025-01-18 18:07:36 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,31 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' || browser.name === 'edge') {
+          launchOptions.args.push('--use-fake-device-for-media-stream');  // Optional for fake media
+          launchOptions.args.push('--use-fake-ui-for-media-stream');      // Fake UI for permission
+          launchOptions.args.push('--enable-geolocation');
+          launchOptions.args.push('--allow-insecure-localhost');          // Optional for local testing
+          launchOptions.args.push('--auto-select-desktop-capture-source=Entire screen');
+
+          // Set default geolocation
+          launchOptions.preferences.default = {
+            'profile': {
+              'content_settings': {
+                'exceptions': {
+                  'geolocation': {
+                    '[*.]localhost:5173': {
+                      'setting': 1 // Allow geolocation for localhost
+                    }
+                  }
+                }
+              }
+            }
+          };
+        }
+        return launchOptions;
+      });
     },
   },
 });
